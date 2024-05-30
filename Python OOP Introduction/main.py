f181a933702d5bfb8ec3e0598c1ef619b8b8dcfa
edmalfizeo <edmalfizeo@gmail.com>
2024-05-29 21:12:13 -0300

    Fixing OOP Turtle

--- Python OOP Introduction/main.py
+++ Python OOP Introduction/main.py
@@ -1,14 +1,34 @@
-import turtle
+import random
 from turtle import *
+import random
 
 timmy = Turtle()
 my_screen = Screen()
 timmy.shape('turtle')
 timmy.color('darkolivegreen')
+timmy.speed(10)
+timmy.setpos(0, 0)
+my_screen.setup(width=800, height=600)
 
-for i in range(20):
-    timmy.forward(100)
-    timmy.left(140)
+for i in range(1000):
+    timmy.pencolor(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
 
+    timmy.pensize(random.randint(1, 5))
+
+    side = random.randint(1, 200)
+
+    for j in range(5):
+        if timmy.xcor() >= 401:
+            timmy.penup()
+            timmy.goto(-400, timmy.ycor())
+            timmy.pendown()
+        if timmy.ycor() >= 300:
+            timmy.penup()
+            timmy.goto(timmy.xcor(), -300)
+            timmy.pendown()
+        timmy.forward(side)
+        timmy.left(120)
+        timmy.forward(side)
+        timmy.right(120)
 
 my_screen.exitonclick()
